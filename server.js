ad4b0992a8e3fb35dd896516ab4359d91f07b906
salhi <salhinfo404@gmail.com>
2022-08-27 05:00:44 +0100

    Access-Control-Allow-Origin 1

--- server.js
+++ server.js
@@ -20,7 +20,24 @@ var corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(function (req, res, next) {
 
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', origin);
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 // parse requests of content-type - application/json
 app.use(express.json());
 
